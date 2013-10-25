63e15a37783d4b076a40319d6c6e5b88e59a25e7
oshimo_yuya <oshimo_yuya@cyberagent.co.jp>
2013-10-25 17:58:57 +0900

    add ios app and modify package.json

---
+++ ios/kirakira-kingyo/kirakira-kingyo/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  kirakira-kingyo
+//
+//  Created by A12642 on 2013/10/24.
+//  Copyright (c) 2013年 yuyaohshimo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+
+@end
