fe0de4404efe7880ef483e7ae415dd8003d7614b
suzuki_shogo <suzuki_shogo@cyberagent.co.jp>
2013-11-08 23:20:19 +0900

    twitter処理追加

--- public/js/main.js
+++ public/js/main.js
@@ -61,7 +61,8 @@ $.ready(function () {
 		}
 		kingyo.Socket = function (view) {
 			var self = this;
-			self.ws = new WebSocket('ws://localhost:8888'); // need to override
+
+			self.ws = new WebSocket('ws://172.22.242.251:8888'); // need to override
 			// self.ws = new WebSocket('ws://172.22.242.251:8888'); // need to override
 			// self.ws = new WebSocket('ws://172.22.247.45:8888');
 			// onを使おうかな
